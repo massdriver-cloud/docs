c4d7204c49b2876e88f1885ba15546a5a1b79e3f
Cory O'Daniel <cory@coryodaniel.com>
2022-10-19 15:04:59 -0700

    posthog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,6 +83,14 @@ module.exports = {
   },
   plugins: [
     [
+      "posthog-docusaurus",
+      {
+        apiKey: process.env.POSTHOG_API_KEY || "DEV",
+        appUrl: "https://app.posthog.com",
+        enableInDevelopment: false
+      },
+    ],
+    [
       // https://github.com/cmfcmf/docusaurus-search-local
       "@cmfcmf/docusaurus-search-local",
       {
