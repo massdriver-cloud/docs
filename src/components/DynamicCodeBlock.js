d2b6ea482df34d04ea4cba46bfda8006142613ca
mclacore <mclacore@gmail.com>
2024-08-28 11:08:24 -0700

    Finished dynamic AWS, working on azure

--- src/components/DynamicCodeBlock.js
+++ src/components/DynamicCodeBlock.js
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 import { UUIDContext } from "./UUIDFetcher";
+import { CredentialContext } from "./CredentialInput";
 
-const DynamicCodeBlock = () => {
+const DynamicTrustPolicy = () => {
   const uuid = useContext(UUIDContext);
-  const code = `aws iam create-role --role-name=massdriver-provisioner --description="Massdriver Cloud Provisioning Role" --assume-role-policy-document='{"Version":"2012-10-17","Statement":[{"Sid":"MassdriverCloudProvisioner","Effect":"Allow","Principal":{"AWS":["308878630280"]},"Action":"sts:AssumeRole","Condition":{"StringEquals":{ "sts:ExternalId":"${uuid}"}}}]}'
+  const { credName } = useContext(CredentialContext);
+
+  const code = `aws iam create-role --role-name=${credName} --description="Massdriver Cloud Provisioning Role" --assume-role-policy-document='{"Version":"2012-10-17","Statement":[{"Sid":"MassdriverCloudProvisioner","Effect":"Allow","Principal":{"AWS":["308878630280"]},"Action":"sts:AssumeRole","Condition":{"StringEquals":{ "sts:ExternalId":"${uuid}"}}}]}'
   `;
 
   return (
@@ -13,4 +16,28 @@ const DynamicCodeBlock = () => {
   );
 };
 
-export default DynamicCodeBlock;
+const DynamicRolePrivileges = () => {
+  const { credName } = useContext(CredentialContext);
+
+  const code = `aws iam attach-role-policy --role-name=${credName} --policy-arn=arn:aws:iam::aws:policy/AdministratorAccess`;
+
+  return (
+    <pre>
+      <code>{code}</code>
+    </pre>
+  );
+};
+
+const DynamicServicePrincipal = () => {
+  const { credName, accountId } = useContext(CredentialContext);
+
+  const code = `az ad sp create-for-rbac --name ${credName} --role contributor --scopes /subscriptions/${accountId}`;
+
+  return (
+    <pre>
+      <code>{code}</code>
+    </pre>
+  );
+};
+
+export { DynamicTrustPolicy, DynamicRolePrivileges, DynamicServicePrincipal };
